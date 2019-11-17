14d9e7efa4a02c342a1d72e2fa721ce752ae71d3
john kazakopoulos <johnpkaza@gmail.com>
2019-11-18 00:32:36 +0200

    Last fixes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
-import GlobalStyles from 'styledComponents/Container';
-import './App.css';
 
 class App extends Component {
   render() {
     const { children } = this.props;
 
-    return (
-      <div id="app-container">
-        <GlobalStyles />
-        {children}
-      </div>
-    );
+    return <div id="app-container">{children}</div>;
   }
 }
 
